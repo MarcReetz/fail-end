d2d52cb8be23ce0a1929a0a88d25b3e8991407af
archtec <1675628-archtec@users.noreply.gitlab.com>
2022-09-20 19:21:14 +0200

    remove old files

--- src/pages/Login/Logout.js
+++
@@ -1,9 +0,0 @@
-import { useAuth } from "../../hooks/useAuth";
-import { useEffect } from "react";
-export default function Logout () {
-  const { logout } = useAuth();
-  useEffect(() => {
-    logout();
-  }, []);
-  return <h1>you have been logtout </h1>
-}
